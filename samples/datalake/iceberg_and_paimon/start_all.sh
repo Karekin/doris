9f346b5e7763685c45234832a2ef1909774c47f5
Karekin <445923692@qq.com>
2024-10-14 15:31:52 +0800

    fix(Doris)：修改启动脚本

--- samples/datalake/iceberg_and_paimon/start_all.sh
+++ samples/datalake/iceberg_and_paimon/start_all.sh
@@ -19,7 +19,7 @@
 
 set -e
 
-DORIS_PACKAGE=apache-doris-2.1.5-bin-x64
+DORIS_PACKAGE=apache-doris-2.1.5-bin-arm64
 DORIS_DOWNLOAD_URL=https://apache-doris-releases.oss-accelerate.aliyuncs.com
 
 download_source_file() {
@@ -56,7 +56,7 @@ fi
 cd packages || exit
 
 download_source_file "${DORIS_PACKAGE}.tar.gz" "0af6706854cedff46ee2210bd949e2e9" "${DORIS_DOWNLOAD_URL}"
-download_source_file "jdk-8u202-linux-x64.tar.gz" "0029351f7a946f6c05b582100c7d45b7" "https://repo.huaweicloud.com/java/jdk/8u202-b08"
+download_source_file "jdk-8u421-macosx-aarch64.tar.gz" "3b6a6468febebf8dfb3480c22a3e11a4" "https://download.oracle.com/otn/java/jdk/8u421-b11/jdk-8u421-macosx-aarch64.tar.gz"
 download_source_file "iceberg-aws-bundle-1.5.2.jar" "7087ac697254f8067d0f813521542263" "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-aws-bundle/1.5.2"
 download_source_file "iceberg-flink-runtime-1.18-1.5.2.jar" "8e895288e6770eea69ea05ffbc918c1b" "https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-flink-runtime-1.18/1.5.2"
 download_source_file "flink-connector-jdbc-3.1.2-1.18.jar" "5c99b637721dd339e10725b81ccedb60" "https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/3.1.2-1.18"
@@ -66,16 +66,17 @@ download_source_file "paimon-spark-3.5-0.8.0.jar" "963d0c17d69034ecf77816f64863f
 download_source_file "flink-shaded-hadoop-2-uber-2.8.3-10.0.jar" "f6f0be5b9cbebfd43e38121b209f4ecc" "https://repo1.maven.org/maven2/org/apache/flink/flink-shaded-hadoop-2-uber/2.8.3-10.0"
 download_source_file "flink-s3-fs-hadoop-1.18.0.jar" "60b75e0fdc5ed05f1213b593c4b66556" "https://repo1.maven.org/maven2/org/apache/flink/flink-s3-fs-hadoop/1.18.0"
 
-if [[ ! -f "jdk1.8.0_202/SUCCESS" ]]; then
+if [[ ! -f "jdk1.8.0_421.jdk/SUCCESS" ]]; then
     echo "Prepare jdk8 environment"
-    if [[ -d "jdk1.8.0_202" ]]; then
-        echo "Remove broken jdk1.8.0_202"
-        rm -rf jdk1.8.0_202
+    if [[ -d "jdk1.8.0_421.jdk" ]]; then
+        echo "Remove broken jdk1.8.0_421.jdk"
+        rm -rf jdk1.8.0_421.jdk
     fi
-    echo "Unpackage jdk1.8.0_202"
-    tar xzf jdk-8u202-linux-x64.tar.gz
-    touch jdk1.8.0_202/SUCCESS
+    echo "Unpackage jdk1.8.0_421.jdk"
+    tar xzf jdk-8u421-macosx-aarch64.tar.gz
+    touch jdk1.8.0_421.jdk/SUCCESS
 fi
+
 
 if [[ ! -f "doris-bin/SUCCESS" ]]; then
     echo "Prepare ${DORIS_PACKAGE} environment"
